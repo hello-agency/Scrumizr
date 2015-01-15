30b90d543fddb6464d6f200fdbcbb53c3ba6bb75
Valentin Druon <valentin.druon@gmail.com>
2015-01-15 16:37:12 +0100

    Edites money.rb to fit with Mongoid 3 (mongoize, demongoize, evolve methods)

--- app/models/money.rb
+++ app/models/money.rb
@@ -1,12 +1,15 @@
 class Money
-  include Mongoid::Fields::Serializable
   
-  def deserialize(object)
+  def demongoize(object)
     object ? (::BigDecimal.new(object.to_s) / 100) : object
   end
   
-  def serialize(object)
+  def mongoize(object)
     object ? ((object.is_a?(::BigDecimal) ? object : ::BigDecimal.new(object.to_s)).round(2) * 100).to_i : object
   end
   
+  def evolve(object)
+    object ? ((object.is_a?(::BigDecimal) ? object : ::BigDecimal.new(object.to_s)).round(2) * 100).to_i : object
+  end
+
 end
