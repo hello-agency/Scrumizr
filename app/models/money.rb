02394357708db4b5d1558fa577155ba959992fc5
Valentin Druon <valentin.druon@gmail.com>
2015-01-15 16:52:51 +0100

    Fixed model : Money, mongoid serialization methods. Money is now stored as cents

--- app/models/money.rb
+++ app/models/money.rb
@@ -1,15 +1,20 @@
 class Money
   
-  def demongoize(object)
-    object ? (::BigDecimal.new(object.to_s) / 100) : object
-  end
-  
-  def mongoize(object)
-    object ? ((object.is_a?(::BigDecimal) ? object : ::BigDecimal.new(object.to_s)).round(2) * 100).to_i : object
-  end
-  
-  def evolve(object)
-    object ? ((object.is_a?(::BigDecimal) ? object : ::BigDecimal.new(object.to_s)).round(2) * 100).to_i : object
+  def mongoize
+    self * 100
   end
 
+  class << self
+    def demongoize(object)
+      object ? (::BigDecimal.new(object.to_s) / 100) : object
+    end
+
+    def mongoize(object)
+      object ? ((object.is_a?(::BigDecimal) ? object : ::BigDecimal.new(object.to_s)).round(2) * 100).to_i : object
+    end
+
+    def evolve(object)
+      object ? ((object.is_a?(::BigDecimal) ? object : ::BigDecimal.new(object.to_s)).round(2) * 100).to_i : object
+    end
+  end
 end
