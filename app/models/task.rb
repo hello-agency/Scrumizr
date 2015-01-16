96adc122d3d6a61d967f20c0cb88124c1a93a673
Valentin Druon <valentin.druon@gmail.com>
2015-01-16 14:32:35 +0100

    Added trackable to Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,4 +7,11 @@ class Task
   has_many :member_tasks
 
   enum :state, [:todo, :doing, :done]
+
+  track_history   :on => [:all],
+                  :modifier_field => :member,
+                  :version_field => :version,
+                  :track_create   =>  true,
+                  :track_update   =>  true,
+                  :track_destroy  =>  true
 end
