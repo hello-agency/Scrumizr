25a2bf90196178ce0062f865d8717ae3f6076dc3
Valentin Druon <valentin.druon@gmail.com>
2015-01-16 11:05:04 +0100

    Fixed space problem in Member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,9 +3,11 @@ class Member
   
   field :fullname, type: String
   field :mail, type: String
-  field :avatar, type :String
-  field :access, type :String
+  field :avatar, type: String
+  field :access, type: String
   field :account_status, type: String
   has_many :project_members
   has_many :member_tasks
+
+
 end
