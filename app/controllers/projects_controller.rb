bd586028f1378dcd9a325a661d2584e7aefe5b55
Valentin Druon <valentin.druon@gmail.com>
2015-01-19 12:24:01 +0100

    Added end for a condition in ProjectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -38,7 +38,8 @@ class ProjectsController < ApplicationController
     if @project.save 
       render json: {status: true, message: 'Project has been successfully destroyed'}
     else
-      render json: {status: true, message: 'Project couldn\'t be deleted, try again later'}  
+      render json: {status: true, message: 'Project couldn\'t be deleted, try again later'}
+    end
   end
   
   private
