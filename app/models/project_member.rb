b835aa8f516b3ae24c61b00c21140119d2a0c1f6
Valentin Druon <valentin.druon@gmail.com>
2015-01-19 17:14:39 +0100

    Added validations to ProjectMember model

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -6,4 +6,10 @@ class ProjectMember
   belongs_to :project
 
   enum :role, [:team_member, :product_owner, :scrum_master]
+
+  validates_uniqueness_of :role, scope: [:member, :project], message: ' must be unique in project'
+  validates_uniqueness_of :member, scope: [:project], message: ' is already in project'
+  validates_presence_of :role
+  validates_presence_of :member
+  validates_presence_of :project
 end
