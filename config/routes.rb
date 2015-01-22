9bf1408dfb77559d4349cb8eedc26c850e483d10
Valentin Druon <valentin.druon@gmail.com>
2015-01-22 22:24:38 +0100

    Added route for project_members#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     delete '/:id', to: 'projects#destroy'
     get '/:project_id/members', to: 'project_members#project_members', as: 'project_members'
     post '/:project_id/members/add', to: 'project_members#create', as: 'project_members_add'
+    delete '/:project_id/members/destroy', to: 'project_members#destroy', as: 'project_members_destroy'
   end
 
   scope '/members' do
